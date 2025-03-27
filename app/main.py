6f0dcb382a7263ade191faedab2310d5b01d530a
engrahsaninam <83651389+engrahsaninam@users.noreply.github.com>
2025-03-27 13:12:03 +0500

    msg

--- app/main.py
+++ app/main.py
@@ -3,17 +3,13 @@ import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from .routes import twilio_route, auth_route, subscription_route
 from .models.base import Base
-from .db.session import engine, init_db
+from .db.session import engine
 
 
 app = FastAPI(
     title="TimeGlobe WhatsApp Assistant",
     description="This Project aims to automate appointment booking through whatsapp",
 )
-
-# Initialize database
-init_db()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -21,7 +17,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+Base.metadata.create_all(bind=engine)
 app.include_router(router=twilio_route.router, prefix="/api/twilio", tags=["Twilio"])
 app.include_router(
     router=auth_route.router, prefix="/api/auth", tags=["authentication"]
@@ -31,6 +27,5 @@ app.include_router(
     prefix="/api/subscriptions",
     tags=["Subscriptions"],
 )
-
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0")
