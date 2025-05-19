4029ac00c7f8103f3dd7ec817e74440675e9ee1d
engrahsaninam <engrahsaninam@gmail.com>
2025-05-19 23:05:36 +0500

    msg

--- app/core/config.py
+++ app/core/config.py
@@ -66,7 +66,7 @@ class Settings(BaseSettings):
     API_BASE_URL: str = env.get("API_BASE_URL", "")
     EMAIL_SENDER: str = env.get("EMAIL_SENDER", "")
 
-    FRONTEND_RESET_PASSWORD_URL="localhost:3000"
+    FRONTEND_RESET_PASSWORD_URL: str = "localhost:3000"
 
     # This is the important part - allows extra fields
     model_config = {
